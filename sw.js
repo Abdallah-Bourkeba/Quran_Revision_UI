c791d206ea7dd53f1665f904c3c8b79f37348d32
Abdallah Bourkeba <87373032+Abdallah-Bourkeba@users.noreply.github.com>
2026-07-20 07:51:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "quran-pwa-v1";
+const urlsToCache = ["./", "./index.html", "./manifest.json", "./icon.png"];
+
+// تثبيت ملفات الكاش
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+// استرجاع الملفات من الكاش ليعمل بدون إنترنت (لواجهة التطبيق)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
